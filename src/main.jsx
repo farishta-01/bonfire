80873b9383c16ed2f8cf581512d54ae37714cd63
farishta-01 <122055202+farishta-01@users.noreply.github.com>
2024-02-01 00:53:40 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter >
+  <BrowserRouter basename='/' >
   <React.StrictMode>
     
      <App />
